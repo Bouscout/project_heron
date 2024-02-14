18222c4cf82e10c18dece597ab4d7ce1ce28fe75
Bouscout <boudiallo1030@gmail.com>
2024-02-14 07:58:53 -0500

    added languageSwaper, and expwebframework command in deploy firebase

---
+++ src/components/footer/change_lang/language_swap.tsx
@@ -0,0 +1,14 @@
+
+
+import "./language_swap.scss"
+
+const LanguageSwap = () => {
+    return (
+        <div id="language-swap">
+            <a href="/">EN</a>
+            <a href="/fr">FR</a>
+        </div>
+    )
+}
+
+export default LanguageSwap
